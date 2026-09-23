e324c182dee07541d6217772a3b4fae6ed22dab3
Juson, Jann Francis <jusonjannfrancis@gmail.com>
2026-09-23 20:50:24 +0800

    Added specific folders for each components of the HomeScreen UI

---
+++ src/Screens/HomeScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen(){
+    
+}
